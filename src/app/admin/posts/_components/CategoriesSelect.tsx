a4f5450c782ba4840caeb7be34f95a5db729d355
shuheitachikawa <shuhei.tusx@gmail.com>
2024-08-29 11:02:23 +0900

    s

--- src/app/admin/posts/_components/CategoriesSelect.tsx
+++ src/app/admin/posts/_components/CategoriesSelect.tsx
@@ -1,57 +1,67 @@
-import * as React from 'react'
-import Box from '@mui/material/Box'
-import OutlinedInput from '@mui/material/OutlinedInput'
-import MenuItem from '@mui/material/MenuItem'
-import FormControl from '@mui/material/FormControl'
-import Select from '@mui/material/Select'
-import Chip from '@mui/material/Chip'
-import { Category } from '@/types/Category'
-import { useEffect } from 'react'
+import * as React from "react";
+import Box from "@mui/material/Box";
+import OutlinedInput from "@mui/material/OutlinedInput";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
+import Chip from "@mui/material/Chip";
+import { Category } from "@/types/Category";
+import { useEffect } from "react";
+import { useSupabaseSession } from "@/app/_hooks/useSupabaseSession";
 
 interface Props {
-  selectedCategories: Category[]
-  setSelectedCategories: (categories: Category[]) => void
+  selectedCategories: Category[];
+  setSelectedCategories: (categories: Category[]) => void;
 }
 
 export const CategoriesSelect: React.FC<Props> = ({
   selectedCategories,
   setSelectedCategories,
 }) => {
-  const [categories, setCategories] = React.useState<Category[]>([])
+  const [categories, setCategories] = React.useState<Category[]>([]);
+
+  const { token } = useSupabaseSession();
 
   const handleChange = (value: number[]) => {
     value.forEach((v: number) => {
-      const isSelect = selectedCategories.some((c) => c.id === v)
+      const isSelect = selectedCategories.some((c) => c.id === v);
       if (isSelect) {
-        setSelectedCategories(selectedCategories.filter((c) => c.id !== v))
-        return
+        setSelectedCategories(selectedCategories.filter((c) => c.id !== v));
+        return;
       }
 
-      const category = categories.find((c) => c.id === v)
-      if (!category) return
-      setSelectedCategories([...selectedCategories, category])
-    })
-  }
+      const category = categories.find((c) => c.id === v);
+      if (!category) return;
+      setSelectedCategories([...selectedCategories, category]);
+    });
+  };
 
   useEffect(() => {
-    const fetcher = async () => {
-      const res = await fetch('/api/admin/categories')
-      const { categories } = await res.json()
-      setCategories(categories)
-    }
+    if (!token) return;
 
-    fetcher()
-  }, [])
+    const fetcher = async () => {
+      const res = await fetch("/api/admin/categories", {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: token,
+        },
+      });
+      const { categories } = await res.json();
+      setCategories(categories);
+    };
+
+    fetcher();
+  }, [token]);
 
   return (
     <FormControl className="w-full">
       <Select
         multiple
         value={selectedCategories}
-        onChange={(e) => handleChange((e.target.value as unknown) as number[])}
+        onChange={(e) => handleChange(e.target.value as unknown as number[])}
         input={<OutlinedInput />}
         renderValue={(selected: Category[]) => (
-          <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: 0.5 }}>
+          <Box sx={{ display: "flex", flexWrap: "wrap", gap: 0.5 }}>
             {selected.map((value: Category) => (
               <Chip key={value.id} label={value.name} />
             ))}
@@ -65,5 +75,5 @@ export const CategoriesSelect: React.FC<Props> = ({
         ))}
       </Select>
     </FormControl>
-  )
-}
+  );
+};
