431410500407fe094cec9d6ccc67542a41547058
shuheitachikawa <shuhei.tusx@gmail.com>
2025-06-18 22:10:39 +0900

    s

--- src/app/_hooks/useSupabaseSession.ts
+++ src/app/_hooks/useSupabaseSession.ts
@@ -5,7 +5,7 @@ import { useState, useEffect } from 'react'
 
 export const useSupabaseSession = () => {
   const pathname = usePathname()
-  // undefind: ログイン状態ロード中, null: ログインしていない, Session: ログインしている
+  // undefind: ログイン状態ロード中, null: ログインしていない, Session: ログインしている.
   const [session, setSession] = useState<Session | null | undefined>(undefined)
   const [token, setToken] = useState<string | null>(null)
   const [isLoding, setIsLoding] = useState(true)
